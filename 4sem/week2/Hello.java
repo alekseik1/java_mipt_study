091c7150c2baf2efcd67eb9732dbf316ff8f9d65
alekseik1 <alekseik1@yandex.ru>
2018-02-15 12:09:30 +0300

    Fibbonnachchii

--- 4sem/week2/Hello.java
+++ 4sem/week2/Hello.java
@@ -1,6 +1,11 @@
 package week2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
+
+    static List<Integer> res = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
@@ -9,6 +14,16 @@ public class Hello {
         p.x = 5;
         p.y = 2;
         System.out.println(p);
+        for(int i=1; i<10; i++)
+            System.out.println(Fib(i));
+    }
+
+    public static int Fib(int n) {
+        if(n == 1 || n == 2) {
+            return 1;
+        } else {
+            return Fib(n-1) + Fib(n-2);
+        }
     }
 
     private static void hello_for() {
