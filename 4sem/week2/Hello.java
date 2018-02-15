6497ac9370a0aa9cdb617c6404cca0e2235fbd72
alekseik1 <alekseik1@yandex.ru>
2018-02-15 09:54:59 +0300

    Five times "Hello, world!"

--- 4sem/week2/Hello.java
+++ 4sem/week2/Hello.java
@@ -4,5 +4,12 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        hello_for();
+    }
+
+    private static void hello_for() {
+        for(int i=0; i < 5; i++) {
+            System.out.println("Hello, world!");
+        }
     }
 }
