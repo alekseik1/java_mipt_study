737254451d1869bf39f834b165b1c5afe9723944
alekseik1 <alekseik1@yandex.ru>
2018-02-15 12:00:12 +0300

    Point2D in Hello class

--- 4sem/week2/Hello.java
+++ 4sem/week2/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         hello_for();
+        Point2D p = new Point2D();
+        p.x = 5;
+        p.y = 2;
+        System.out.println(p);
     }
 
     private static void hello_for() {
