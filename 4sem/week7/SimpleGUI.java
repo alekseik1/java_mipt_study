302682d4b4cee9d91ece4023650ce7370f5f2684
alekseik1 <alekseik1@yandex.ru>
2018-03-22 11:37:33 +0300

    SimpleGUI

---
+++ 4sem/week7/SimpleGUI.java
@@ -0,0 +1,73 @@
+package week7;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Line2D;
+
+public class SimpleGUI extends JFrame {
+    private Color color;
+
+    public SimpleGUI() {
+        setSize(450, 450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        color = Color.RED;
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+
+        JButton button = new JButton("Run!");
+        panel.add(button);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                color = Color.BLUE;
+                SimpleGUI.this.repaint();
+            }
+        });
+        button.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                color = Color.GREEN;
+                SimpleGUI.this.repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                color = Color.BLACK;
+                SimpleGUI.this.repaint();
+            }
+        });
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(color);
+        Line2D lin = new Line2D.Float(100, 100, 250, 260);
+        g2.draw(lin);
+    }
+
+    public static void main(String[] args) {
+        SimpleGUI gui = new SimpleGUI();
+        gui.setVisible(true);
+    }
+}
