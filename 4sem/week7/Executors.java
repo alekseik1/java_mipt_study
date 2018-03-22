3c58a22c883ec23e872087766b8d7f50118d3df7
alekseik1 <alekseik1@yandex.ru>
2018-03-22 11:28:08 +0300

    ExecutorService with SingleThreadExecutor

--- 4sem/week7/Executors.java
+++ 4sem/week7/Executors.java
@@ -1,4 +1,21 @@
 package week7;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
 public class Executors {
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = java.util.concurrent.Executors.newSingleThreadExecutor();
+        Callable<Integer> callable = () -> {
+            System.out.println("Hello");
+            return 0;
+        };
+        Future<Integer> future = executor.submit(callable);
+        future.get();
+        future.isDone();
+        future.isCancelled();
+        future.cancel(true);
+    }
 }
