b9965c827d557a19d7108d5b8e439c1c8559a8c0
alekseik1 <alekseik1@yandex.ru>
2018-04-12 11:50:59 +0300

    Стакал код. Сейчас буду улучшать

--- 4sem/week10/GameGUI.java
+++ 4sem/week10/GameGUI.java
@@ -1,63 +1,139 @@
 package week10;
 
-import javafx.util.Pair;
-import week7.SimpleGUI;
-
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.geom.Line2D;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class GameGUI extends JFrame {
-    private Color color;
-    private float t = 0;
+public class GameGUI {
 
-    public GameGUI() {
-        GameOfLife game = new GameOfLife(5, 5, true);
-        Pair<Integer, Integer>[] init_cells = new Pair[]{new Pair(2, 3), new Pair(2, 4), new Pair(1, 3), new Pair(1, 4)};
-        game.initialize(init_cells);
+    private final String NAME_OF_GAME = "Conway's Game of Life";
+    private final int START_LOCATION = 100;
+    private final int LIFE_SIZE = 250;
+    private final int LIFE_SIZE_WIDTH = 250;
+    private final int LIFE_SIZE_HEIGHT = 250;
+    private int showDelay = 5;
 
-        setSize(450, 450);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        color = Color.RED;
-        JPanel panel = new JPanel();
-        getContentPane().add(panel);
-
-        JButton button = new JButton("Run!");
-        panel.add(button);
-
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                color = Color.BLUE;
-                GameGUI.this.repaint();
-                ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-                Runnable task = () -> {
-                    game.makeEvolution();
-                    GameGUI.this.repaint();
-                };
-                executor.scheduleAtFixedRate(task, 0, 1000, TimeUnit.MILLISECONDS);
-            }
-        });
-    }
-
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(color);
-        Line2D lin = new Line2D.Float((float) (100*(2+Math.cos(t/50))), (float) (100*(Math.sin(t/50)+2)), 250, 250);
-        g2.draw(lin);
-    }
+    private final int POINT_RADIUS = 3;
+    private final int BTN_PANEL_HEIGHT = 48;
+    private boolean[][] lifeGeneration = new boolean[LIFE_SIZE][LIFE_SIZE];
+    private boolean[][] nextGeneration = new boolean[LIFE_SIZE][LIFE_SIZE];
+    private volatile boolean startGUINextGeneration = false; // fixed the problem in 64-bit JVM
+    private Canvas canvasPanel;
+    private Random random = new Random();
 
     public static void main(String[] args) {
-        GameGUI gui = new GameGUI();
-        gui.setVisible(true);
+        new GameGUI().startGUI();
+    }
+
+    private void startGUI() {
+        JFrame frame = new JFrame(NAME_OF_GAME);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        int FIELD_SIZE = LIFE_SIZE * POINT_RADIUS + 7;
+        frame.setSize(FIELD_SIZE, FIELD_SIZE + BTN_PANEL_HEIGHT);
+        frame.setLocation(START_LOCATION, START_LOCATION);
+        frame.setResizable(true);
+
+        canvasPanel = new Canvas();
+        canvasPanel.setBackground(Color.white);
+
+        JButton fillButton = new JButton("Fill");
+        fillButton.addActionListener(new FillButtonListener());
+
+        JButton stepButton = new JButton("Step");
+        stepButton.addActionListener((ActionEvent e) -> {
+            processOfLife();
+            canvasPanel.repaint();
+        });
+
+        final JButton startGUIButton = new JButton("Play");
+        startGUIButton.addActionListener(e -> {
+            startGUINextGeneration = !startGUINextGeneration;
+            startGUIButton.setText(startGUINextGeneration? "Stop" : "Play");
+        });
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(fillButton);
+        btnPanel.add(stepButton);
+        btnPanel.add(startGUIButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH, btnPanel);
+        frame.setVisible(true);
+
+        // endless loop of life
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+        Runnable run_gui = () -> {
+            if (startGUINextGeneration) {
+                processOfLife();
+                canvasPanel.repaint();
+            }
+        };
+        service.scheduleAtFixedRate(run_gui, 0, showDelay, TimeUnit.MILLISECONDS);
+    }
+
+    // randomly fill cells
+    public class FillButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            for (int x = 0; x < LIFE_SIZE; x++) {
+                for (int y = 0; y < LIFE_SIZE; y++) {
+                    lifeGeneration[x][y] = random.nextBoolean();
+                }
+            }
+            canvasPanel.repaint();
+        }
+    }
+
+    // count the number of neighbors
+    private int countNeighbors(int x, int y) {
+        int count = 0;
+        for (int dx = -1; dx < 2; dx++) {
+            for (int dy = -1; dy < 2; dy++) {
+                int nX = x + dx;
+                int nY = y + dy;
+                nX = (nX < 0) ? LIFE_SIZE - 1 : nX;
+                nY = (nY < 0) ? LIFE_SIZE - 1 : nY;
+                nX = (nX > LIFE_SIZE - 1) ? 0 : nX;
+                nY = (nY > LIFE_SIZE - 1) ? 0 : nY;
+                count += (lifeGeneration[nX][nY]) ? 1 : 0;
+            }
+        }
+        if (lifeGeneration[x][y]) { count--; }
+        return count;
+    }
+
+    // the main process of life
+    private void processOfLife() {
+        for (int x = 0; x < LIFE_SIZE; x++) {
+            for (int y = 0; y < LIFE_SIZE; y++) {
+                int count = countNeighbors(x, y);
+                nextGeneration[x][y] = lifeGeneration[x][y];
+                // if are 3 live neighbors around empty cells - the cell becomes alive
+                nextGeneration[x][y] = (count == 3) || nextGeneration[x][y];
+                // if cell has less than 2 or greater than 3 neighbors - it will be die
+                nextGeneration[x][y] = ((count >= 2) && (count <= 3)) && nextGeneration[x][y];
+            }
+        }
+        for (int x = 0; x < LIFE_SIZE; x++) {
+            System.arraycopy(nextGeneration[x], 0, lifeGeneration[x], 0, LIFE_SIZE);
+        }
+    }
+
+    // paint on the canvas
+    public class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            for (int x = 0; x < LIFE_SIZE; x++) {
+                for (int y = 0; y < LIFE_SIZE; y++) {
+                    if (lifeGeneration[x][y]) {
+                        g.fillOval(x*POINT_RADIUS, y*POINT_RADIUS, POINT_RADIUS, POINT_RADIUS);
+                    }
+                }
+            }
+        }
     }
 }
